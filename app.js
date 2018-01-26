d584a8046826e3f036eee082d2a103ff997a1b57
Brianna <bripiz@gmail.com>
2018-01-26 11:06:22 -0800

    assignment 6 UXintesive : complete

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 function onReady() {
   const toDos = [];
   const addToDoForm = document.getElementById('addToDoForm');
+  const toDoList = document.getElementById('toDoList');
+  //1 created an id vaiable and then set it to 0
+  let id = 0;
+
 
   function createNewToDo() {
     const newToDoText = document.getElementById('newToDoText');
@@ -8,10 +12,13 @@ function onReady() {
 
     toDos.push({
       title: newToDoText.value,
-      complete: false
+      complete: false,
+      // 2 - add id property and set it to store the value of id
+      //3 and incrament it ++
+      id: id++
     });
     newToDoText.value = '';
-
+    console.log(id);
     renderTheUI();
   }
 
@@ -23,22 +30,59 @@ function onReady() {
     toDos.forEach(function(toDo) {
       const newLi = document.createElement('li');
       const checkbox = document.createElement('input');
+      //4 create a delete button
+      const deleteButton = document.createElement('button');
+
       checkbox.type = "checkbox";
+      // 4 of type button
+      deleteButton.type = "button";
+      // deleteButton.id = "deleteButton";
+      deleteButton.innerHTML = "delete To Do";
 
       newLi.textContent = toDo.title;
 
       toDoList.appendChild(newLi);
       newLi.appendChild(checkbox);
-    });
+      //4 append it to newLi
+      newLi.appendChild(deleteButton);
 
+      deleteButton.addEventListener ("click", event =>{
+        event.preventDefault();
+
+        function removeItem() {
+          for (var i = 0; i < toDos.length; i++ ){
+               if( toDo.id < i && id == i){
+                 return true;
+               }else{
+                 event.target.parentElement.remove()
+               }
+            }
+        }
+        console.log(removeItem());
+        console.log(toDo.id);
+        console.log( toDos.filter(removeItem));
+        return toDos.filter(removeItem);
+        renderTheUI()
+      });
+
+
+    });
   }
+
 
   addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
     createNewToDo();
   });
 
+  // //4 add event listener
+  // toDoList.addEventListener('click', event => {
+  //   alert('dod it');
+  //
+  // });
+
   renderTheUI();
+
 }
 
 window.onload = function() {
